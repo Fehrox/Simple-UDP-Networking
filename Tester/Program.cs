34bb6d8a9d878d1401fda09ae5e4eb0f826972f5
Sebastiaan Fehr <SebFehr@gmail.com>
2014-03-31 17:06:29 +1000

    Received views are applied to their routed targets, instead re-assigning references, and losing registered connections.

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -6,13 +6,19 @@ namespace Tester
     
     class Program
     {
-        static ViewTester viewTesterOne = new ViewTester(1, "1", true);
+        static ViewTester viewTesterOne = new ViewTester(1, "One", true);
+        static ViewTester viewTesterTwo = new ViewTester(2, "Two", false);
         static void Main(string[] args) {
             Network.Listen();
+            viewTesterOne.ViewID = 1;
+            viewTesterTwo.ViewID = 2;
             while (true) {
                 Console.WriteLine("Press any key to send message.");
                 Console.ReadKey();
+                viewTesterOne.TestInt++;
+                viewTesterTwo.TestInt++;
                 viewTesterOne.ServerAssert();
+                viewTesterTwo.ServerAssert();
             }
         }
     }
--- UnityPrototyping/Assets/NetTester.cs
+++ UnityPrototyping/Assets/NetTester.cs
@@ -4,15 +4,26 @@ using Network = UdpLanViews.Network.Network;
 
 public class NetTester : MonoBehaviour {
 
-    ViewTester viewTesterOne = new ViewTester(1, "One", true);
+    ViewTester viewTesterOne = new ViewTester(1, 1, "DesktopOne");
+    ViewTester viewTesterTwo = new ViewTester(2, 1, "DesktopTwo");
 
     void Start() {
         Network.Listen();
+        viewTesterOne.TestInt = 1;
+        viewTesterOne.TestString = "DesktopOne";
+        viewTesterTwo.TestInt = 2;
+        viewTesterTwo.TestString = "DesktopTwo";
     }
 
     void OnGUI() {
+        GUILayout.Label("viewTesterOne " + viewTesterOne.TestString + " " + viewTesterOne.TestInt);
+        GUILayout.Label("viewTesterTwo " + viewTesterTwo.TestString + " " + viewTesterTwo.TestInt);
+
         if (GUILayout.Button("RUN THAT BIATCH!!!")) {
+            viewTesterOne.TestInt++;
+            viewTesterTwo.TestInt++;
             viewTesterOne.ServerAssert();
+            viewTesterTwo.ServerAssert();
         }
     }
 
