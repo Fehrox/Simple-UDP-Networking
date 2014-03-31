3453de2ba850fbe49b9e3f8246a15eca9dc2873a
Seb Fehr <SebFehr@gmail.com>
2014-03-31 11:33:25 +1000

    Project foundations.

---
+++ UdpLanViews/Views/View.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+using UdpLanViews.Network;
+
+namespace UdpLanViews.Views
+{
+    /// <summary>
+    /// Classes derived from View can be syncronised with other 
+    /// listening instances on the local network with ServerAssert().
+    /// </summary>
+    public abstract class View : ISerializable
+    {
+        /// <summary>
+        /// When many instances of a given derived class exist, 
+        /// the view id determines which a routed view instance
+        /// should be applied to.
+        /// </summary>
+        public ushort ViewID = 0;
+
+        /// <summary>
+        /// Syncronise this instance of the parent of this derived 
+        /// View with other view instances on the network.
+        /// </summary>
+        public void ServerAssert() {
+            NetworkSend.Send(this);
+        }
+
+        protected View() {
+            ViewRouting.RegisterView(this);
+        }
+
+        ~View() {
+            ViewRouting.UnRegisterView(this);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            info.AddValue("ViewID", ViewID, typeof(ushort));
+        }
+
+        protected View(SerializationInfo info, StreamingContext context) {
+            ViewID = info.GetUInt16("ViewID");
+        }
+    }
+
+}
---
+++ UnityPrototyping/Assets/ViewTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Runtime.Serialization;
+using UdpLanViews.Views;
+
+[Serializable]
+public class ViewTester : View, ISerializable
+{
+
+    public bool TestBool;
+    public int TestInt;
+    public string TestString;
+    public ViewTester(int testInt, string testString, bool testBool) {
+        TestInt = testInt;
+        TestString = testString;
+        TestBool = testBool;
+    }
+
+    void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context) {
+        info.AddValue("TestBool", TestBool);
+        info.AddValue("TestInt", TestInt);
+        info.AddValue("TestString", TestString);
+    }
+
+    public ViewTester(SerializationInfo info, StreamingContext context) {
+        TestBool = info.GetBoolean("TestBool");
+        TestInt = info.GetInt32("TestInt");
+        TestString = info.GetString("TestString");
+        Debug.Log(TestBool + " " + TestInt + " " + TestString + " recieved object");
+    }
+}
